7166a7b11902afa8ed47543b451b384c86c11774
Alejandro Ramirez Garcia <76931084+ale9420@users.noreply.github.com>
2023-07-05 15:36:06 -0500

    added env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    supabaseUrl: 'https://vmnjzdvtfjmecunqisyh.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtbmp6ZHZ0ZmptZWN1bnFpc3loIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NjMyMjUwMywiZXhwIjoyMDAxODk4NTAzfQ.XOuLsa7p7U1XrdEPAZPXtmR3RdH-hosmCpk02cKKzwo',
-
+  supabaseUrl: '{SUPABASE_URL}',
+  supabaseKey: '{SUPABASE_KEY}',
 };
